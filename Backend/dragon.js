7d1da57d49842cbf6e40af4de53645301a85889e
Brandon2797 <brgdude@gmail.com>
2021-11-15 12:12:51 -0500

    adding DEFAULT_PROPERTIES to Dragon class

--- Backend/dragon.js
+++ Backend/dragon.js
@@ -1,10 +1,16 @@
+const DEFAULT_PROPERTIES = {
+    nickname: 'unnamed',
+    birthday: new Date()
+}
+
+
 class Dragon {
   constructor({birthday, nickname}) {
     //  We can make this map or rather an object by surrounding the arguments itself with a pair of curly braces.
-    
-    this.birthday = birthday;
 
-    this.nickname = nickname;
+    this.birthday = birthday || DEFAULT_PROPERTIES.birthday;
+
+    this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
   }
 }
 
