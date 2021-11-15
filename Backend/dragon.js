8a954e6fe1d45ec50e4974dfb8e0614f0acb79e7
Brandon2797 <brgdude@gmail.com>
2021-11-15 12:14:16 -0500

    specify that this new date is the birth date and nickname is a sting

--- Backend/dragon.js
+++ Backend/dragon.js
@@ -1,4 +1,6 @@
 const DEFAULT_PROPERTIES = {
+    //defual properties in cases where  no nickname or birhtdate vlues 
+    
     nickname: 'unnamed',
     birthday: new Date()
 }
@@ -9,6 +11,7 @@ class Dragon {
     //  We can make this map or rather an object by surrounding the arguments itself with a pair of curly braces.
 
     this.birthday = birthday || DEFAULT_PROPERTIES.birthday;
+    // in no birthday or nickname value is defind it will set to defualt using the Or indication using the two Pipe symbols 
 
     this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
   }
